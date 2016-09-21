9352f902d26f7a5731970dc6b84a8d54aad9fee0
Thomas Wade <w1220w@126.com>
2016-09-21 18:53:39 +0800

    sync

--- js/getHCJ.js
+++ js/getHCJ.js
@@ -1,37 +1,89 @@
-//html
-						console.log($("#float-r").prop("outerHTML"));
-						//js
-						console.log($("#float-r").next("script").text());
+
++function ($) {
+  'use strict';
+
+  function getHCJ(element, options) {
+       //传入的triggerbtn
+       this.triggerBtn = $(element);
+
+       this.getSelector(this.triggerBtn);
+  }
+
+  getHCJ.DEFAULTS = {
+    
+  }	
+  getHCJ.prototype.getSelector = function(triggerBtn){
+  	   var h = triggerBtn.parent().next(".box");
+  	   this.getH(h)	
+  }
+  getHCJ.prototype.getH = function (h){
+    var that = this;
+	
+	console.log($(h).html());
+  }
+  getHCJ.prototype.getC = function () {
+    var that = this;
+	function css(selector){
+
+		function readCss(a){
+
+		    var sheets = document.styleSheets, o = [];
+		    a.matches = a.matches || a.webkitMatchesSelector || a.mozMatchesSelector || a.msMatchesSelector || a.oMatchesSelector;
+		    for (var i in sheets) {
+		        var rules = sheets[i].rules || sheets[i].cssRules;
+		        for (var r in rules) {
+		            if (a.matches(rules[r].selectorText)) {
+		                o[rules[r].selectorText] = (rules[r].cssText);
+		            }
+		        }
+		    }
+		    return o;
+		};
+		
+		function writeCss(){
+
+		};
+
+		var ele = $(selector)[0];
+		var cssArray = readCss(ele);
+		
+	};
+	
+  }
+  getHCJ.prototype.getJ = function () {
+    var that = this;
+	//js
+	console.log($("#float-r").next("script").text());
+	
+  }
+  
+  function Plugin(option) {
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('hcj.get')
+      var options = typeof option == 'object' && option
+
+      if (!data) $this.data('hcj.get', (data = new getHCJ(this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  var old = $.fn.set
+
+  $.fn.getHCJ             = Plugin
+  $.fn.getHCJ.Constructor = getHCJ
+
+  $.fn.set.noConflict = function () {
+    $.fn.getHCJ = old
+    return this
+  }
+
+}(jQuery);						
+
 						
-						function css(selector){
-
-							function readCss(a){
-
-							    var sheets = document.styleSheets, o = [];
-							    a.matches = a.matches || a.webkitMatchesSelector || a.mozMatchesSelector || a.msMatchesSelector || a.oMatchesSelector;
-							    for (var i in sheets) {
-							        var rules = sheets[i].rules || sheets[i].cssRules;
-							        for (var r in rules) {
-							            if (a.matches(rules[r].selectorText)) {
-							                o[rules[r].selectorText] = (rules[r].cssText);
-							            }
-							        }
-							    }
-							    return o;
-							};
-							
-							function writeCss(){
-
-							};
-
-							var ele = $(selector)[0];
-							var cssArray = readCss(ele);
-							console.log(cssArray);
-						};
+						
+						
+						
 						
 
-						$(function(){
-							css(".float-r");
-							css(".heart");
-						})
 						
--- js/index.js
+++ js/index.js
@@ -1,7 +1,78 @@
-$(function(){
-	setButton();
-});
-function setButton(){
-    var btnRow = $('<div class="headRow clear"><span class="button btn-r-m bg-pink white float-l"><img style="margin-top: -3px;" src="/img/icon_w.png" class="icon">Get!</span></div>')	
-	$(".box").prepend(btnRow );
-}
+
++function ($) {
+  'use strict';
+
+  function set(element, options) {
+    this.container = $(element);
+    this.box = $(element).find(".box");
+    this.options = $.extend({}, set.DEFAULTS, options);
+    this.ini();
+  }
+
+  set.DEFAULTS = {
+    trigger: ".get"
+  }	
+
+  set.prototype.ini = function (){
+    var that = this;
+	if(this.box != 0){
+		var btnRow = $('<div class="headRow clear"><span class="button btn-r-m bg-pink white float-l get"><img style="margin-top: -3px;" src="/img/icon_w.png" class="icon">Get!</span></div>')	
+		
+		this.box.before(btnRow);
+		$.getScript("/js/getHCJ.js",function(){
+			that.modalWindow();
+		});
+		
+	}
+  }
+  set.prototype.modalWindow = function () {
+    var that = this;
+    
+	$(document).on("click",that.options.trigger,function(){
+		  var $trigger = $(this);
+		  layer.open({
+			  type: 1,
+			  skin: 'demo-class',  //给弹窗添加特殊的class设置样式
+			  area: ['800px', '560px'],
+			  shadeClose: true, //点击遮罩关闭
+			  content:'<div class="line hcj"><div id="html" class="block block-3"></div><div id="css" class="block block-3"></div><div id="js" class="block block-3"></div></div>',
+		  	  success: function(layero, index){
+			    		 $trigger.getHCJ();
+			  }	
+		  });
+	})
+	
+  }
+  
+  function Plugin(option) {
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('hcj.set')
+      var options = typeof option == 'object' && option
+
+      if (!data) $this.data('hcj.set', (data = new set(this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  var old = $.fn.set
+
+  $.fn.set             = Plugin
+  $.fn.set.Constructor = set
+
+  $.fn.set.noConflict = function () {
+    $.fn.set = old
+    return this
+  }
+
+  // SCROLLSPY DATA-API
+  // ==================
+
+  $(window).on('load.hcj.set.data-api', function () {
+    $('[data-set="set"]').each(function () {
+      var $set = $(this)
+      Plugin.call($set, $set.data())  //#set.data()获取元素上的data-xxx的内容
+    })
+  })
+
+}(jQuery);
